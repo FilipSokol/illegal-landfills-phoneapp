b0d2636df507ccf2cac76cdfb11b095739bd6832
FilipSokol <sok.filip@gmail.com>
2022-07-31 22:32:11 +0200

    add login form

--- App.js
+++ App.js
@@ -1,29 +1,6 @@
-import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+// screens
+import Login from './screens/Login';
 
 export default function App() {
-  const [test, setTest] = useState("");
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Tst</Text>
-      <input
-        onChange={(e) => {
-          setTest(e.target.value);
-        }}
-      ></input>
-      {test}
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Login />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ screens/Login.js
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import { View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { Formik } from 'formik';
+
+// icons
+import { Octicons, Ionicons } from '@expo/vector-icons';
+
+// components
+import {
+  StyledContainer,
+  InnerContainer,
+  PageLogo,
+  PageTitle,
+  SubTitle,
+  StyledFormArea,
+  LeftIcon,
+  RightIcon,
+  StyledInputLabel,
+  StyledTextInput,
+  Colors,
+} from '../components/styles';
+
+// colors
+const { brand, darkLight } = Colors;
+
+const Login = () => {
+  const [hidePassword, setHidePassword] = useState(true);
+
+  return (
+    <StyledContainer>
+      <StatusBar style="dark" />
+      <InnerContainer>
+        <PageLogo resizeMode="cover" source={require('../assets/logo.jpg')} />
+        <PageTitle>Praca Inżynierska</PageTitle>
+        <SubTitle>Logowanie</SubTitle>
+
+        <Formik
+          initialValues={{ email: '', password: '' }}
+          onSubmit={(values) => {
+            console.log(values);
+          }}
+        >
+          {({ handleChange, handleBlur, handleSubmit, values }) => (
+            <StyledFormArea>
+              <MyTextInput
+                label="Email"
+                icon="mail"
+                onChangeText={handleChange('email')}
+                onBlur={handleBlur('email')}
+                value={values.email}
+                keyboardType="email-address"
+              />
+              <MyTextInput
+                label="Hasło"
+                icon="lock"
+                onChangeText={handleChange('password')}
+                onBlur={handleBlur('password')}
+                value={values.password}
+                secureTextEntry={hidePassword}
+                isPassword={true}
+                hidePassword={hidePassword}
+                setHidePassword={setHidePassword}
+              />
+            </StyledFormArea>
+          )}
+        </Formik>
+      </InnerContainer>
+    </StyledContainer>
+  );
+};
+
+const MyTextInput = ({ label, icon, isPassword, hidePassword, setHidePassword, ...props }) => {
+  return (
+    <View>
+      <LeftIcon>
+        <Octicons name={icon} size={30} color={brand} />
+      </LeftIcon>
+      <StyledInputLabel>{label}</StyledInputLabel>
+      <StyledTextInput {...props} />
+      {isPassword && (
+        <RightIcon onPress={() => setHidePassword(!hidePassword)}>
+          <Ionicons name={hidePassword ? 'md-eye-off' : 'md-eye'} size={30} color={darkLight} />
+        </RightIcon>
+      )}
+    </View>
+  );
+};
+
+export default Login;
